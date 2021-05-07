383c040d4da42f58b7a5ef7ed40c0b3b865e82d1
047 <992083825@qq.com>
2021-05-07 19:56:35 +0800

    zy

--- testcase/Test_04titan.py
+++ testcase/Test_04titan.py
@@ -1,17 +1,17 @@
-# import json
-# import os
-# import allure
-# import pytest
-# from common.handle_test import Handle
-# from config import Conf
-# from utils.YamlUtil import YamlReader
-#
-# getDataIndexValueTreeList = os.path.join(Conf.get_data_path(), "TITAN_getDataIndexValueTreeList.yml")
-# data_index_value_tree_list = YamlReader(getDataIndexValueTreeList).read_data_all()
+import json
+import os
+import allure
+import pytest
+from common.handle_test import Handle
+from config import Conf
+from utils.YamlUtil import YamlReader
+
+getDataIndexValueTreeList = os.path.join(Conf.get_data_path(), "TITAN_getDataIndexValueTreeList.yml")
+data_index_value_tree_list = YamlReader(getDataIndexValueTreeList).read_data_all()
 
 
-# @allure.feature("Titan筛选")
-# class TestTitan:
-#     @pytest.mark.parametrize("index_value_tree_list", data_index_value_tree_list)
-#     def test_data_index_value_tree_list(self, index_value_tree_list):
-#         Handle().handle_test(index_value_tree_list)
+@allure.feature("Titan筛选")
+class TestTitan:
+    @pytest.mark.parametrize("index_value_tree_list", data_index_value_tree_list)
+    def test_data_index_value_tree_list(self, index_value_tree_list):
+        Handle().handle_test(index_value_tree_list)
